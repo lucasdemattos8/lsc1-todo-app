77355d54a6f9a5a7ccbf52a0995a64ee8b9fed7f
LucasMiranda <ldemattosmiranda@gmail.com>
2024-12-31 23:07:28 -0300

    #5 - Cria as classes de entidades da aplicação

---
+++ backend/src/main/java/com/lsc/todo_app/domain/entity/Task.java
@@ -0,0 +1,88 @@
+package com.lsc.todo_app.domain.entity;
+
+import java.time.LocalDateTime;
+
+import com.lsc.todo_app.domain.entity.enums.Status;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ backend/src/main/java/com/lsc/todo_app/domain/entity/enums/Status.java
@@ -0,0 +1,7 @@
+package com.lsc.todo_app.domain.entity.enums;
+
+public enum Status {
+    PENDING,
+    COMPLETED,
+    DELETED;
+}
