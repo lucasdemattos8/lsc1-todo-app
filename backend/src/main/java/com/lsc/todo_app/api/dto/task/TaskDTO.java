c228d453af72cde31443bb0f2a3e45b7021b70a2
LucasMiranda <ldemattosmiranda@gmail.com>
2025-01-02 08:25:37 -0300

    Adiciona DTOs para exposição de dados

---
+++ backend/src/main/java/com/lsc/todo_app/api/dto/task/TaskDTO.java
@@ -0,0 +1,84 @@
+package com.lsc.todo_app.api.dto.task;
+
+import java.time.LocalDateTime;
+
+import com.lsc.todo_app.api.dto.user.UserSumarryDTO;
+import com.lsc.todo_app.domain.entity.Task;
+import com.lsc.todo_app.domain.entity.enums.Status;
+
+public class TaskDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private Status status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private UserSumarryDTO user;
+
+    public TaskDTO() {
+    }
+
+    public TaskDTO(Long id, String title, String description, Status status, LocalDateTime createdAt, LocalDateTime updatedAt, UserSumarryDTO user) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.user = user;
+    }
+
+    public TaskDTO(Task task, UserSumarryDTO user) {
+        this.id = task.getId();
+        this.title = task.getTitle();
+        this.description = task.getDescription();
+        this.status = task.getStatus();
+        this.createdAt = task.getCreatedAt();
+        this.updatedAt = task.getUpdatedAt();
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    public UserSumarryDTO getUser() {
+        return user;
+    }
+    public void setUser(UserSumarryDTO user) {
+        this.user = user;
+    }
+}
---
+++ backend/src/main/java/com/lsc/todo_app/api/dto/user/UserDTO.java
@@ -0,0 +1,69 @@
+package com.lsc.todo_app.api.dto.user;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.lsc.todo_app.api.dto.task.TaskSumarryDTO;
+import com.lsc.todo_app.domain.entity.User;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private List<TaskSumarryDTO> task;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.task = user.getTask().stream()
+                .map(TaskSumarryDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<TaskSumarryDTO> getTask() {
+        return task;
+    }
+
+    public void setTask(List<TaskSumarryDTO> task) {
+        this.task = task;
+    }
+}
