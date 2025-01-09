67f4e301c72e3e5efe559a6c9d4bab1893142308
LucasMiranda <ldemattosmiranda@gmail.com>
2025-01-09 13:36:11 -0300

    Adiciona anotações de exibição aos testes de contexto

--- backend/src/test/java/com/lsc/todo_app/TodoAppApplicationTests.java
+++ backend/src/test/java/com/lsc/todo_app/TodoAppApplicationTests.java
@@ -1,12 +1,15 @@
 package com.lsc.todo_app;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@DisplayName("Tests for loading Spring context and database")
 @SpringBootTest
 class TodoAppApplicationTests {
 
 	@Test
+	@DisplayName("Should load the Spring context and database correctly")
 	void contextLoads() {
 	}
 
